cf321864c5308922f32fe266c3de5e4c58018a10
ProgramerNawaf <blibddesmond@gmail.com>
2023-07-19 17:21:27 +0300

    comitted

---
+++ controllers/products.js
@@ -0,0 +1,28 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    prods: Product.getAll(),
+    pageTitle: "Shop",
+    path: "/",
+    hasProducts: Product.getAll().length > 0,
+    activeShop: true,
+    productCSS: true,
+  });
+};
